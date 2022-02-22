3e7c61283f5b437e26131486f9de6698923c83ed
Oleg Kholyk <oleg@kholyk.ru>
2022-02-22 15:05:15 +0300

    Quotes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "hexlet_code"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'hexlet_code'
 
-require "minitest/autorun"
+require 'minitest/autorun'
 
 def get_fixture_content(fixture)
   file = File.new("test/fixtures/#{fixture}.html")

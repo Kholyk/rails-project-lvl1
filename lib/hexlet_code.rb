8f1ae688ce5276a0376898e5a7219779650f2de3
Oleg Kholyk <i@kholyk.ru>
2024-03-22 09:51:37 +0300

    module inside parent module to able to call HexletCode::Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,5 +5,5 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
   autoload :Tag, "hexlet_code/tag"
-  class Tag < Tag; end
+
 end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-class Tag
-  def self.build(name, attributes = {}, &block)
-    tag = []
-    tag << "<#{name}#{render_attributes(attributes)}>"
-    return tag.join if tag_single?(name)
+module HexletCode
+  module Tag
+    def self.build(name, attributes = {}, &block)
+      tag = []
+      tag << "<#{name}#{render_attributes(attributes)}>"
+      return tag.join if tag_single?(name)
 
-    tag << yield if block
-    tag << "</#{name}>"
-    tag.join
-  end
+      tag << yield if block
+      tag << "</#{name}>"
+      tag.join
+    end
 
-  def self.tag_single?(tag_name)
-    singles = %w[img br input hr]
-    singles.include? tag_name
-  end
+    def self.tag_single?(tag_name)
+      singles = %w[img br input hr]
+      singles.include? tag_name
+    end
 
-  def self.render_attributes(attributes = {})
-    attributes.collect { |key, value| " #{key}=\"#{value}\"" }.join
+    def self.render_attributes(attributes = {})
+      attributes.collect { |key, value| " #{key}=\"#{value}\"" }.join
+    end
   end
 end
